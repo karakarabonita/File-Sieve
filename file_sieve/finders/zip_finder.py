a2c77b54cd007346f2d29cb699eb4f08799ff960
karakarabonita <108755567+karakarabonita@users.noreply.github.com>
2023-07-03 12:54:55 -0400

    Rename ZIPData class in accord with other finders

--- file_sieve/finders/zip_finder.py
+++ file_sieve/finders/zip_finder.py
@@ -2,14 +2,14 @@ from util.file_data_util import check_cross_sector_footer
 from formats.hf_finder import HFFinder
 
 
-class ZIPData(HFFinder):
+class ZIPFinder(HFFinder):
     header = b'\x50\x4B\x03\x04'
     footer = b'\x50\x4B\x05\x06'
     remainder_len = 18
 
     def __init__(self, exclude, out_dir, make_new=True):
         super().__init__(
-            ZIPData.header, ZIPData.footer, 'zip', out_dir, make_new=make_new)
+            ZIPFinder.header, ZIPFinder.footer, 'zip', out_dir, make_new=make_new)
         self.exclude = exclude
         self.out_dir = out_dir
 
@@ -21,14 +21,14 @@ class ZIPData(HFFinder):
         while len(sector) > 0:
             if self.footer in sector:
                 byte_count += sector.index(self.footer) + len(self.footer) + \
-                    ZIPData.remainder_len
+                    ZIPFinder.remainder_len
                 return True, byte_count
 
             next_sector = f.read(512)
             additional_bytes = check_cross_sector_footer(
                 sector, next_sector, self.footer)
             if additional_bytes > 0:
-                byte_count += additional_bytes + ZIPData.remainder_len
+                byte_count += additional_bytes + ZIPFinder.remainder_len
                 return True, byte_count
             
             sector = next_sector
